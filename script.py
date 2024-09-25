1d981015407a72e3417856093a2e62282daad11a
youssefhanyr <163347478+youssefhanyr@users.noreply.github.com>
2024-09-25 14:50:15 +0300

    Documented

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ basicConfig(level=INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 COOKBOOK = {'onigiri':'210000', 'califonia_roll':'111000', 'gunmaki':'112000', 'sushi':'110020', 'shrimpushi': '110200', 'unagshi':'110002', 'dragon_roll':'211002'\
             , 'combo':'211111'}                                   
-# order of ingrediants will be the following, rice, nori, feggs, shrimps, salmon, unagi
+# order of ingredients will be the following, rice, nori, feggs, shrimps, salmon, unagi
 DUR = 0.15
 
 
@@ -73,9 +73,9 @@ def getpath(Picname: str) -> str:
 
 class Kitchenbot:
 
-    def __init__(self) -> None:
-        region = getregion_coords()
-        self.didcall = False
+    def __init__(self) -> None:                        # making one for lacking and another for unavailabilty is so we call ahead when its low, it then gets marked
+        region = getregion_coords()                    # as lacking to prevent stopping cooking and calling again, and unavailable, when ingred is low it cant
+        self.didcall = False                           # cant be used to make most dishes
         self.gamecoords = (round(region[0] - 640/1.57), round(region[1] - 480/1.87), 640, 480)
         print(f"{self.gamecoords[0]}, {self.gamecoords[1]}")
         self.inventory = {'shrimp': 5, 'rice': 10, 'nori': 10, 'feggs': 10, 'salmon': 5, 'unagi': 5}
@@ -99,28 +99,35 @@ class Kitchenbot:
         debug(f'Showing orders before update, {self.seatordersplate} \n {self.seatorderstime} \n {self.seatskipped}')
         info('Comparing recevied orders..')
         for seat in [f'seat{i}' for i in range(1, 7)]:
-            plate_travel_time = 3.5*(int(seat[-1]) - 1) if int(seat[-1]) > 1 else 0
+            plate_travel_time = 3.5*(int(seat[-1]) - 1) if int(seat[-1]) > 1 else 0 # plate travelling time for seats 2,3,..
             debug(f"Plate travel time is {plate_travel_time}..")
+                    
             if self.seatorderstime[seat] is None:
                 self.seatorderstime[seat] = round(time(), 2)
+                        
             if (round(time(), 2) - self.seatorderstime[seat] > 5 + 5 + plate_travel_time + 0.5 and foodlist[seat] == self.prevseatordersplate[seat] \
                 and self.prevseatordersplate[seat] is not None) or (self.seatskipped[seat] and foodlist[seat] is not None):
+                            # 5 sec to reach the first seat, 5 (with dishes with 4 pieces its more) eating animation, and 0.5 to account for lost time(est)
                 self.seatordersplate[seat] = foodlist[seat]
                 self.seatorderstime[seat] = timelist[seat]
                 debug(f'Added {foodlist[seat]} to the foodlist because it has been left over.. ')
-            elif self.seatordersplate[seat] != foodlist[seat] and self.prevseatordersplate[seat] != foodlist[seat]:
+                            
+            elif self.seatordersplate[seat] != foodlist[seat] and self.prevseatordersplate[seat] != foodlist[seat]:       # takes a new order if its different
                 debug(f'Changed {seat} order from {self.seatordersplate[seat]} to {foodlist[seat]}')
                 self.seatordersplate[seat] = foodlist[seat]
                 self.seatorderstime[seat] = timelist[seat]
                 self.prevseatordersplate[seat] = self.seatordersplate[seat]
+                        
             else:
                 self.seatordersplate[seat] = None
                 debug('Recieved same order, nullifing...')
+                        
         debug(f'Showing orders after update {self.seatordersplate} \n {self.seatorderstime} \n {self.seatskipped}')
         info('Starting cooking each plate..')
+                
         for seat in [f'seat{i}' for i in range(1, 7)]:
             if self.seatordersplate[seat] is not None:
-                debug(f'Cooking {self.seatordersplate[seat]}')
+                info(f'Cooking {self.seatordersplate[seat]}')
                 self.cook_plate(self.seatordersplate[seat][:-4], seat)
 
     def collect_plates(self) -> None:
@@ -141,14 +148,15 @@ class Kitchenbot:
             if intingrd_number != 0:
                 if self.ingred_unavilable[ingreds[ing_number]]:
                     info(f'{ingreds[ing_number]} is currently unavailable, skipping doing {plate}.. ')
-                    self.seatskipped[seat] = True
-                    self.checkandcall()
+                    self.seatskipped[seat] = True           # this dict is to make seats skipped due to lack of ingrd get taken instantlly when they arrive
+                    self.checkandcall()                  
                     return
         
         for ing_number in range(6):
             intingrd_number = int(recipe[ing_number])
             if intingrd_number != 0:
-                for _ in range(intingrd_number):
+                for _ in range(intingrd_number):            # The clicking field
+                            
                     if ingreds[ing_number] == 'rice':
                         pyautogui.click(self.gamecoords[0] + 120, self.gamecoords[1] + 417, duration=DUR)
                         debug('Clicked on rice..')
@@ -170,12 +178,12 @@ class Kitchenbot:
                     self.inventory[ingreds[ing_number]] -= 1
             else:
                 continue
-        debug('sleeping before clicking on the mat..')
+        debug('sleeping before clicking on the mat..')            # to try to prevent overclocking the belts from occuring
         sleep(0.6)
         pyautogui.click(self.gamecoords[0] + 259, self.gamecoords[1] + 488, duration=DUR)
         info(f'Clicked on the mat, {plate} is done..')
         debug('wait for mat animation..')
-        sleep(0.8)
+        sleep(0.8)                        # didn't measure the anim time, this is an est
         self.seatordersplate[seat] = None
         self.seatskipped[seat] = False
         self.checkandcall()
@@ -189,8 +197,10 @@ class Kitchenbot:
         """
         for ingred, amount in self.inventory.items():
             info(f'Checking the state of {ingred}, current amount is {amount}...')
-            if ingred in ('rice', 'nori', 'feggs'):
-                if amount <= 2 and not self.ingred_lacking[ingred]:
+            if ingred in ('rice', 'nori', 'feggs'):                        # The ones that arrive in tens
+                        
+                if amount <= 2 and not self.ingred_lacking[ingred]:            # not lacking as in didnt call already
+                            
                     info(f'Calling for {ingred}..')
                     self.call(ingred)
                 elif amount < 2 and self.ingred_lacking[ingred]:
@@ -198,7 +208,9 @@ class Kitchenbot:
                     debug(f"{ingred} is already called for, marking unavailable...")
                 else:
                     debug(f'{ingred} is good..')
-            elif ingred in ('shrimp', 'salmon', 'unagi'):
+                            
+            elif ingred in ('shrimp', 'salmon', 'unagi'):            # The ones that arrive in fives
+                        
                 if amount <= 2 and not self.ingred_lacking[ingred]:
                     info(f'Calling for {ingred}..')
                     self.call(ingred)
@@ -217,6 +229,7 @@ class Kitchenbot:
         """
         debug(f'Current inventory: {self.inventory}')
         if not mode:
+                    
             info('Resting inventory and seats...')
             self.inventory = {'shrimp': 5, 'rice': 10, 'nori': 10, 'feggs': 10, 'salmon': 5, 'unagi': 5}
             self.seatordersplate = {'seat1': None, 'seat2': None, 'seat3': None, 'seat4': None, 'seat5': None, 'seat6': None}
@@ -224,40 +237,49 @@ class Kitchenbot:
             self.ingred_lacking = {'shrimp': False, 'rice': False, 'nori': False, 'feggs': False, 'salmon': False, 'unagi': False}
             self.ingred_unavilable = {'shrimp': False, 'rice': False, 'nori': False, 'feggs': False, 'salmon': False, 'unagi': False}
             self.seatskipped = {'seat1': False, 'seat2': False, 'seat3': False, 'seat4': False, 'seat5': False, 'seat6': False}
+                    
         else:
+                    
             for ingred, amount in self.inventory.items():
+                        
                 if amount < 0:
-                    critical(f'Shit just hit the fan, {ingred} is currently at the count: {amount}')
-                elif amount == 0:
-                    info(f'{ingred} has run out, disabling all dishes that needs it.. ')
+                    critical(f'Shit just hit the fan, {ingred} is currently at the count: {amount}') # while this is bad, its not game-ending, as this means
+                elif amount == 0:                                                                    # next time it calls for ingred, it will call it earlier
+                    info(f'{ingred} has run out, disabling all dishes that needs it.. ')             # and besides, it will get reseted after a win
                     self.ingred_unavilable[ingred] = True
+                            
             info('Checking if any ingredients arrived..')
             for ingred, status in self.ingred_lacking.items():
                 if status and time() - self.ingred_calltime[ingred] >= 7:
+                            
                     if ingred in ('shrimp', 'unagi', 'salmon'):
                         self.inventory[ingred] += 5
                         info(f'{ingred} arrived, increased it by 5')
                         self.ingred_lacking[ingred] = False
                         self.ingred_unavilable[ingred] = False
                         self.didcall = False
+                                
                     else:
                         self.inventory[ingred] += 10
                         info(f'{ingred} arrived, increased it by 10')
                         self.ingred_lacking[ingred] = False
                         self.ingred_unavilable[ingred] = False
                         self.didcall = False
+                                
         debug(f'Updated inventory: {self.inventory}')
                         
     def call(self, ingred: str) -> None:
         pyautogui.click(self.gamecoords[0] + 738, self.gamecoords[1] + 460)             # clicked on the phone
-        toppings = ('shrimp', 'salmon', 'unagi', 'nori', 'feggs')
+        toppings = ('shrimp', 'salmon', 'unagi', 'nori', 'feggs')                        # each button for ingred on the phone menu is ordered accordingly
         toppings_coords = ((626, 284), (627, 426), (730, 284), (627, 353), (731, 352))
 
         if ingred in toppings:
             info(f'Calling for {ingred}..')
             pyautogui.click(self.gamecoords[0] + 691, self.gamecoords[1] + 346, duration=DUR)
+            """Had a huge deal with the module not detecting the unafford picture, it would give false pstvs no matter how much I fiddled so I 
+            made it find the afford one instead"""
             try:
-                pyautogui.locateCenterOnScreen(getpath(f'{ingred}_afford.png'), minSearchTime=0.8, grayscale=False, region=(930, 460, 1140, 700))
+                pyautogui.locateCenterOnScreen(getpath(f'{ingred}_afford.png'), minSearchTime=0.8, grayscale=False, region=(self.gamecoords[0] + 575, self.gamecoords[1] + 223, 210, 240))
                 pyautogui.click(self.gamecoords[0] + toppings_coords[toppings.index(ingred)][0], self.gamecoords[1] + toppings_coords[toppings.index(ingred)][1])
                 pyautogui.click(self.gamecoords[0] + 628, self.gamecoords[1] + 375, duration=DUR)
                 info(f'Called for {ingred}...')
@@ -267,6 +289,7 @@ class Kitchenbot:
                 return
             
             except (pyautogui.ImageNotFoundException, pyscreeze.ImageNotFoundException):
+                        
                 self.ingred_unavilable[ingred] = True
                 info(f'Can\'t afford {ingred}, marking it unavailable..')
                 pyautogui.click(self.gamecoords[0] + 749, self.gamecoords[1] + 436, duration=DUR)
@@ -275,8 +298,9 @@ class Kitchenbot:
         elif ingred == 'rice':
             info(f'Calling for rice..')
             pyautogui.click(self.gamecoords[0] + 693, self.gamecoords[1] + 372, duration=DUR)
+                    
             try:
-                pyautogui.locateCenterOnScreen(getpath(f'rice_afford.png'), grayscale=False, region=(930, 460, 1140, 700), minSearchTime=0.8)
+                pyautogui.locateCenterOnScreen(getpath(f'rice_afford.png'), grayscale=False, region=(self.gamecoords[0] + 575, self.gamecoords[1] + 223, 210, 240), minSearchTime=0.8)
                 pyautogui.click(self.gamecoords[0] + 692, self.gamecoords[1] + 359, duration=DUR)
                 pyautogui.click(self.gamecoords[0] + 628, self.gamecoords[1] + 375, duration=DUR)
                 self.ingred_lacking[ingred] = True
@@ -284,6 +308,7 @@ class Kitchenbot:
                 info(f'Called for {ingred}...')
                 self.didcall = True
                 return
+                        
             except (pyautogui.ImageNotFoundException, pyscreeze.ImageNotFoundException):
 
                 self.ingred_unavilable[ingred] = True
@@ -292,17 +317,21 @@ class Kitchenbot:
                 return
     
     def collect_shit(self):
+        """Collects shit plates made by making wrong recipes, this is to prevent overclogging the belts and making more mistakes"""
         shit_region = (self.gamecoords[0] + 436, self.gamecoords[1] +393, 60, 211)
         info('Looking for shit plates..')
         while True:
+                    
             try:
                 coords = list(pyautogui.locateAllOnScreen(getpath('shit.png'), grayscale=False, confidence=0.9, \
                                                         region=shit_region))
                 info('Found shit plates...')
                 for plate in coords:
+                            
                     xandy_coords = pyautogui.center(plate)
                     pyautogui.click(xandy_coords[0], xandy_coords[1])
                     debug(f'Clicked on {xandy_coords[0]} , {xandy_coords[1]}')
+                            
             except (pyautogui.ImageNotFoundException, pyscreeze.ImageNotFoundException):
                 info('Didn\'t find any, returning.. ')
                 return
@@ -313,6 +342,8 @@ def regentcheck(kitchen: Kitchenbot) -> None:
     I forgot what was this supposed to do
     i remembered, it was supposed to check if any moves are possible and if we broke
     so it doesn't keep looping when it has nothing to do
+
+    UPDATE: made this obsolete, its pretty much useless since in all the times i ran the script it has never needed to go into such state
     """
     unavailable_sum = 0
     if kitchen.didcall:
@@ -354,7 +385,7 @@ def checkgameend(kitchen: Kitchenbot) -> None:
     
 
 if __name__ == '__main__':
-    print('Sushi go round script\n made by cheeto and dorito \n \n \n CURRENTLY ON STANDBY, MAKE SURE THE GAME')
+    print('Sushi go round script\n made by youssefhanyr \n \n \n CURRENTLY ON STANDBY, MAKE SURE THE GAME')
     print('IS PRESENT IN THE BROWSER WINDOW AND NOT ON FULLSCREEN, INPUT ANYTHING TO PROCEED')
     input('')
     print('Will start locating the game..')
